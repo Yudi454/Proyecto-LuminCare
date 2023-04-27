2d0a140a4a361d5b09a6d29aace54aa55d8ef7b3
Yudi454 <lucasyudi445@gmail.com>
2023-04-27 13:56:46 -0300

    Cree un generador de numero random

--- main.js
+++ main.js
@@ -1,44 +1,71 @@
 //Inicio script de bootstrap para validar el formulario
 (() => {
-    'use strict'
-    
-    const forms = document.querySelectorAll('.needs-validation')
-  
-    Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
+  "use strict";
+
+  const forms = document.querySelectorAll(".needs-validation");
+
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
         if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-          document.getElementById("id-form").addEventListener("submit", function(event) {
-            event.preventDefault(); 
-          });
-          
-        } else{
+          event.preventDefault();
+          event.stopPropagation();
+          document
+            .getElementById("id-form")
+            .addEventListener("submit", function (event) {
+              event.preventDefault();
+            });
+        } else {
           //Inicio alerta de sweet alert
-          swal({ 
+          swal({
             title: "Good job!",
             text: "You clicked the button!",
             icon: "success",
             button: "Aww yiss!",
           });
-          vaciardatos()
+          vaciardatos();
           //Fin alerta de sweet alert
         }
-        
-        form.classList.add('was-validated')
-      }, false)
-    })
-  })()
-  //Inicio script de bootstrap para validar el formulario
-  
-  
-      function vaciardatos () {
-      let codigoInput = document.getElementById("codigo")
-      codigoInput.value = "" 
-      let codigoNombre = document.getElementById("name")
-      codigoNombre.value = ""
-      let codigoDescripcion = document.getElementById("descripcion")
-      codigoDescripcion.value = ""
-      let codigoURL = document.getElementById("url")
-      codigoURL.value = ""
-      }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
+//Fin script de bootstrap para validar el formulario
+
+//Inicio funcion para borrar datos luego de enviar el formulario
+function vaciardatos() {
+  let codigoInput = document.getElementById("codigo");
+  codigoInput.value = "";
+  let codigoNombre = document.getElementById("name");
+  codigoNombre.value = "";
+  let codigoDescripcion = document.getElementById("descripcion");
+  codigoDescripcion.value = "";
+  let codigoURL = document.getElementById("url");
+  codigoURL.value = "";
+}
+//Fin funcion para borrar datos luego de enviar el formulario
+
+
+//Inicio generador de numero random
+const numerosGenerados = []
+
+function numerorandom() {
+  let numerorandom = Math.floor(Math.random() * 999999)
+
+  while (numerosGenerados.includes(numerorandom)) {
+    numerorandom = Math.floor(Math.random() * 999999)
+  }
+
+  numerosGenerados.push(numerorandom)
+  return numerorandom
+}
+
+numerorandom()
+numerorandom()
+
+console.log(numerosGenerados)
+//Fin generador de numero random
