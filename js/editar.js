a747b1144b2b94e536bbba3343c18deae2010028
Yudi454 <lucasyudi445@gmail.com>
2023-05-14 19:42:04 -0300

    Se agrego el javascript de Inicio

--- js/editar.js
+++ js/editar.js
@@ -1,22 +1,20 @@
-const obtenerproductos = localStorage.getItem("productos")
+const obtenerproductos = localStorage.getItem("productos");
 
-const cartas = document.getElementById("cartas")
-console.log("Hola Mundo")
-console.log(obtenerproductos)
+const cartas = document.getElementById("cartas");
 
 JSON.parse(obtenerproductos).forEach((producto) => {
-    const carta = `
-    <div class="container-fluid col-md-4" data-item="${producto.categoria}">
+  const carta = `
+    <div class="container-fluid col-md-4 col-lg-4 col-sm-6 productos-item ${producto.categoria}" data-item="${producto.categoria}">
     <div class="card">
       <img src="${producto.url}" class="card-img-top img-fluid" alt="...">
       <div class="card-body">
-        <h5 class="card-title">${producto.nombre}</h5>
+        <h2 class="card-title" id="productos-item -name">${producto.nombre}</h2>
         <p>${producto.categoria}</p>
         <p class="card-text">${producto.descripcion}</p>
-        <a href="#" class="btn btn-primary">${producto.precio}</a>
-        <a href="#" class="btn btn-primary">Producto Deseado</a>
+        <h2 class="productos-item -value">$${producto.precio}</h2>
+        <button href="#" data-filter="${producto.categoria}" data-id="${producto.codigo}" class="btn btn-primary botonAgregar">Producto Deseado</button>
       </div>
     </div>
     `;
-    cartas.innerHTML += carta
-})
+  cartas.innerHTML += carta;
+});
