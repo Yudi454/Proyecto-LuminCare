ca8da57b2323b2eec607a835974de887aa502d9c
Angel0333 <88207677+Angel0333@users.noreply.github.com>
2023-05-14 16:49:34 -0300

    agregando funciones a login

--- js/login.js
+++ js/login.js
@@ -3,6 +3,12 @@ const inputEmail = document.getElementById("email");
 const inputContraseña = document.getElementById("password");
 const form = document.querySelector("form");
 const usuarioRechazado = document.getElementById("usuarioRechazado");
+const divIngreso = document.getElementById("btn-ingreso")
+const divRegistro =  document.getElementById("btn-registro")
+const dropUsuario = document.getElementById("dropdown-usuario")
+const dropNombre = document.getElementById("drop-nombre")
+const dropEmail = document.getElementById("drop-email")
+
 
 let arrayUsuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
 
@@ -21,8 +27,14 @@ form.addEventListener("submit", (e) => {
     }    
     if (bandera === true){
         form.reset();
-        alert("USUARIO ENCONTRADO");
+         alert("USUARIO ENCONTRADO");
+        dropUsuario.style.display = 'block';
+        // divIngreso.style.display = 'none';
+        divRegistro.style.display = 'none';
+        dropNombre.textContent(arrayUsuarios[i].nombre);
+        dropEmail.textContent(arrayUsuarios[i].email);
         usuarioRechazado.innerHTML = "";
+
     } else{
         usuarioRechazado.innerHTML = "<p>Contraseña incorrecta</p>";
     }
