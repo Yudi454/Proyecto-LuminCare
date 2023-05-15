3ffa0699d38a5345b3eabf9f61f0b23c3d6792cf
Yudi454 <lucasyudi445@gmail.com>
2023-05-15 20:46:20 -0300

    Se agrego la funcionalidad al filtro

--- js/editar.js
+++ js/editar.js
@@ -5,19 +5,18 @@ const cartas = document.getElementById("cartas");
 JSON.parse(obtenerproductos).forEach((producto) => {
   const carta = `
     <div class="col-10 col-sm-6 col-lg-4 mx-auto my-3 productos-item ${producto.categoria}" data-item="${producto.categoria}"style="width: 18rem;">
-    <div class="card">
+    <div class="card d-flex align-items-center">
         <div class="contenedorImg">
             <img class="productosImg img-fluid" src="${producto.url}" alt="">
-            <img class="productosImg img-fluid" src="./img/img-productos/exfoleanteverde-superpuesta.png" alt="">                        
+            <img class="productosImg img-fluid" src="${producto.url2}" alt="">                        
         </div>
         <div class="card-body">
-            <div class="card-text d-flex justify-content-between text-capitalize"></
-                <h5 id="productos-item-name">${producto.nombre}</h5>
-                <h5 class="productos-item-value">$<strong id="productos-item-price" class="font-weight-bold">$${producto.precio}</strong></h5>
-            </div>
+                <h5 class="card-title" id="productos-item-name">${producto.nombre}</h5>
+                <p class="card-text">HolaMundo</p>
+                <h5 class="productos-item-value">$<strong id="productos-item-price" class="font-weight-bold">${producto.precio}</strong></h5>
             <div class="contenedorBtnComprar">
-            <button href="#" data-filter="${producto.categoria}" data-id="${producto.codigo}" class="btn btn-primary botonAgregar">Producto Deseado</button>
-                <a href="#"
+            <button href="#" data-filter="${producto.categoria}" data-id="${producto.codigo}" class="btn btn-dark botonAgregar">Producto Deseado</button>
+                <a href="../html/Error404.html"
                 class="btn btn-outline-dark mt-2" style="width: 13em;"
                 data-filter="${producto.categoria}">Añadir al Carrito</a>
             </div>
@@ -29,7 +28,7 @@ JSON.parse(obtenerproductos).forEach((producto) => {
  
 });
 (function () {
-  const botones = document.querySelectorAll('.btn')/*Selecciona todas las clases con el nombre btn*/
+  const botones = document.querySelectorAll('.btn-filter')/*Selecciona todas las clases con el nombre btn*/
   const storeItems = document.querySelectorAll('.productos-item')/*Selecciona todas las clases con el nombre productos-item seria el contenedor de las cards*/
   
   botones.forEach((boton) => {
